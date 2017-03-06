fe68fe56e6e1516b2f0032d5d8d8318586be85c8
Julian <Kadus@Julians-MacBook-Pro.local>
2017-03-06 12:38:39 -0500

    first js exercise completed

--- First_Js_Exercise/script.js
+++ First_Js_Exercise/script.js
@@ -1,5 +1,6 @@
+console.log("From the script")
 var firstName = prompt("What is your first name?");
 var lastName = prompt("What is your last name?");
 var age = prompt("How old are you?");
-alert("Well it's nice to meet " + firstName + " " + lastName + ".");
-alert("You are " + age + " years old.");
+console.log("Well it's nice to meet " + firstName + " " + lastName + ".");
+console.log("You are " + age + " years old.");
